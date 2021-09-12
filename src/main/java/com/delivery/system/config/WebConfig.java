76fde739144416b42377ebbba0e9a997a8fae9f8
Sriyoukan <36580905+Sriyoukan@users.noreply.github.com>
2021-09-12 14:14:43 +0530

    notification

--- src/main/java/com/delivery/system/config/WebConfig.java
+++ src/main/java/com/delivery/system/config/WebConfig.java
@@ -2,12 +2,14 @@ package com.delivery.system.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@CrossOrigin
 public class WebConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
